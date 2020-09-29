be1af943acc9855ddde17348d2275ddb07caac2c
Luke-Gebler <48456872+Luke-Gebler@users.noreply.github.com>
2020-09-29 15:01:02 -0400

    Basic functionality for Board.java as well as small testing function in Main.java

--- CS4341-Project2/src/Board.java
+++ CS4341-Project2/src/Board.java
@@ -1,3 +1,28 @@
 public class Board {
     
+    //Main board
+    private int[][] board;
+
+    //Creates 15x15 board (initialized with 0 in every spot)
+    public Board() {
+        board = new int[15][15];
+    }
+
+    //Adds a piece to the board if no piece exists there yet(board[x][y] == 0)
+        //Currently accepts any # for piece parameter since we haven't decided what numbers we're accpeting (probably 1 and 2 for the two players?)
+    public boolean addPeice(int x, int y, int piece) {
+        boolean flag = false;
+        if(board[x][y] == 0) {
+            board[x][y] = piece;
+            flag = true;
+        }
+        return flag;
+    }
+
+    //Returns the piece (int) of the given coordinate
+    public int getPiece(int x, int y) {
+        int piece = 0;
+        piece = board[x][y];
+        return piece;
+    }
 }
--- CS4341-Project2/src/Main.java
+++ CS4341-Project2/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
+    //Game board
+    static Board board = new Board();
+
+    //Main function
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World");
+        printPiece(5, 4);
+        board.addPeice(5, 4, 1);
+        printPiece(5, 4);
+    }
+
+    //Prints the piece of the board at the given coordinates
+    public static void printPiece(int x, int y) {
+        System.out.println("Piece at " + x + ", " + y + ": " + board.getPiece(x, y));
     }
 }
